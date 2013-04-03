344207e59fa7b5e563e6b68715f4773944f5e5a7
Ayumu AIZAWA <aaizawa@salesforce.com>
2013-04-03 20:16:32 +0900

    add require

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
+require 'heroku-api'
+
 class App < Sinatra::Base
   get '/' do
     heroku = Heroku::API.new(:api_key => request.env["bouncer.token"])
-    @apps = heroku.get_apps
+    @apps = heroku.get_apps.body
     erb :index
   end
 end
