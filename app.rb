b8c01199775154ad68cfd3e33f4607e6a2d5ae78
Ayumu AIZAWA <aaizawa@salesforce.com>
2013-04-03 21:09:44 +0900

    show user

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require 'heroku-api'
 
 class App < Sinatra::Base
+  helpers do
+  	def api
+  	  halt(401) unless request.env["bouncer.token"]
+  	  Heroku::API.new(:api_key => request.env["bouncer.token"])
+  	end
+  end
+
+  before do
+    if request.env['bouncer.user']
+      @user = request.env['bouncer.user']
+    end
+  end
+
   get '/' do
-    heroku = Heroku::API.new(:api_key => request.env["bouncer.token"])
-    @apps = heroku.get_apps.body
+    @apps = api.get_apps.body
     erb :index
   end
 end
